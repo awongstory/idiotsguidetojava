cac1a1fa09ef71b06cbc073fd27bd52eec4432f2
Alicia <9159818+awongstory@users.noreply.github.com>
2022-12-07 11:37:58 -0500

    Create AliciaspringbootApplication.java

---
+++ AliciaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.truthmd.aliciaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliciaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliciaspringbootApplication.class, args);
+	}
+
+}
